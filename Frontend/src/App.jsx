16798ad5414b2cf1ba71514a2969a6d6bc2652e2
SaiManideep00 <saimanideep00@gmail.com>
2024-08-01 10:02:08 -0400

    Initial commit with frontend setup

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Task from "./components/Task";
+
+function App() {
+  return (
+    <div>
+      <h1 className="m-4 p-4">Welcome to Task Manager</h1>
+      <p>Create a Task</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ Frontend/src/components/Task.jsx
@@ -0,0 +1,15 @@
+function Task() {
+  return (
+    <div>
+      <form>
+        <div>
+          <h1>Create a new task</h1>
+          <label htmlFor="task">Name</label>
+          <input type="text" placeholder="Enter task" />
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Task;
